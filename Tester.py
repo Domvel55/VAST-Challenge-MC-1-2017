c97b75594579d23a23dca1fb061550a7b7db4062
Domvel55 <domvel55@gmail.com>
2021-03-02 17:36:53 -0500

    Reduced Code Duplication Combined average_gate_usage_by_weekday() & average_car_type_by_weekday()

--- Tester.py
+++ Tester.py
@@ -4,3 +4,5 @@ from DataManipulator import DataManipulator
 data = DataManipulator('Lekagul Sensor Data.csv')
 data.average_car_type_by_weekday()
 print(data.output)
+data.average_gate_usage_by_weekday()
+print(data.output)
