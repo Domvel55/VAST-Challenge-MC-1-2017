f530ae80959a2aaed74422c0c0808890a5b3c2fc
Domvel55 <domvel55@gmail.com>
2021-03-19 15:37:40 -0400

    Added cars_that_stay()

--- Tester.py
+++ Tester.py
@@ -1,8 +1,7 @@
 from DataManipulator import DataManipulator
+import pandas as pd
 
 
 data = DataManipulator('Lekagul Sensor Data.csv')
-data.average_car_type_by_weekday()
-print(data.output)
-data.average_gate_usage_by_weekday()
-print(data.output)
+
+print(data.cars_that_stayed())
